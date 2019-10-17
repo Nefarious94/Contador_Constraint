dc0c06bc65a0e66cd368566a03be2a6827f30542
sergi <sergi.fs48@gmail.com>
2019-10-17 17:58:08 +0200

    contador relative layout

---
+++ app/src/main/java/com/example/contadorrelativelayout/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.contadorrelativelayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int count = 0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button auto = (Button) findViewById(R.id.auto);
+        Button reset = (Button) findViewById(R.id.reset);
+        final TextView contador = (TextView) findViewById(R.id.contador);
+
+
+        auto.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                count++;
+                contador.setText(Integer.toString(count));
+            }
+        });
+
+        reset.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                count = 0;
+                contador.setText("0");
+            }
+        });
+
+    }
+}
